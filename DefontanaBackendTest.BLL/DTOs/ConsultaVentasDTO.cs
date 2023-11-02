48f946d4a47181002ebade629087ef380ba3941c
Hector Caballero <caballero.22.809@gmail.com>
2023-11-02 11:46:54 -0600

    Add ConsultaVentas endpoint (consulta #1 y #2)

--- DefontanaBackendTest.BLL/DTOs/ConsultaVentasDTO.cs
+++ DefontanaBackendTest.BLL/DTOs/ConsultaVentasDTO.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DefontanaBackendTest.BLL.DTOs
+﻿namespace DefontanaBackendTest.BLL.DTOs
 {
-    public class ConsultaVentasDTO
+    public class Consulta1VentasDTO
     {
         public string? MontoTotalVentasUltimos30Dias { get; set; }
         public int CantidadTotalVentasUltimos30Dias { get; set; }
+    }
+
+    public class Consulta2VentasDTO
+    {
+        public string? DiaVentaMasAlta { get; set; }
+        public string? HoraVentaMasAlta { get; set; }
+        public string? MontoVentaMasAlta { get; set; }
+    }
+
+    public class ConsultaVentasDTO
+    {
+        public Consulta1VentasDTO? Consulta1 { get; set; }
+        public Consulta2VentasDTO? Consulta2 { get; set; }
     }
 }
--- DefontanaBackendTest.BLL/Services/VentasService.cs
+++ DefontanaBackendTest.BLL/Services/VentasService.cs
@@ -3,12 +3,7 @@ using DefontanaBackendTest.BLL.DTOs;
 using DefontanaBackendTest.BLL.Services.Interfaces;
 using DefontanaBackendTest.DAL;
 using DefontanaBackendTest.DL.Entities;
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DefontanaBackendTest.BLL.Services
 {
@@ -27,18 +22,43 @@ namespace DefontanaBackendTest.BLL.Services
         {
             List<VentaDTO> ventas = _mapper.Map<List<VentaDTO>>(await _ventasRepository.GetAllAsync());
 
+            // Consulta 1
             DateTime fechaInicio = DateTime.Now.AddDays(-30);
+            DateTime fechaFin = DateTime.Now;
 
-            var resultado = ventas.Where(v => v.Fecha >= fechaInicio && v.Fecha <= DateTime.Now)
-                .GroupBy(v => 1)
-                .Select(c => new ConsultaVentasDTO
-                {
-                    MontoTotalVentasUltimos30Dias = c.Sum(v => v.Total).ToString("N2", CultureInfo.InvariantCulture),
-                    CantidadTotalVentasUltimos30Dias = c.Count()
-                })
-                .FirstOrDefault();
+            List<VentaDTO> ventasUltimos30Dias = ventas.Where(v => v.Fecha >= fechaInicio && v.Fecha <= fechaFin).ToList();
 
-            return resultado ?? new ConsultaVentasDTO();
+            Consulta1VentasDTO ventasConsulta1 = new Consulta1VentasDTO();
+
+            if (ventasUltimos30Dias.Any())
+            {
+                ventasConsulta1.MontoTotalVentasUltimos30Dias = ventasUltimos30Dias.Sum(v => v.Total).ToString("N2", CultureInfo.InvariantCulture);
+                ventasConsulta1.CantidadTotalVentasUltimos30Dias = ventasUltimos30Dias.Count();
+            }
+            else
+            {
+                ventasConsulta1.MontoTotalVentasUltimos30Dias = "0.00";
+                ventasConsulta1.CantidadTotalVentasUltimos30Dias = 0;
+            }
+
+            // Consulta 2
+            VentaDTO ventaMasAlta = _mapper.Map<VentaDTO>(ventas.OrderByDescending(v => v.Total).FirstOrDefault());
+            Consulta2VentasDTO ventasConsulta2 = new Consulta2VentasDTO();
+            if (ventaMasAlta != null)
+            {
+                ventasConsulta2.DiaVentaMasAlta = ventaMasAlta?.Fecha.ToString("dd 'de' MMMM 'del' yyyy", new CultureInfo("es-ES"));
+                ventasConsulta2.HoraVentaMasAlta = ventaMasAlta?.Fecha.ToString("HH:mm:ss");
+                ventasConsulta2.MontoVentaMasAlta = ventaMasAlta?.Total.ToString("N2", CultureInfo.InvariantCulture);
+            }
+
+            var resultado = new ConsultaVentasDTO
+            {
+                Consulta1 = ventasConsulta1,
+                Consulta2 = ventasConsulta2
+
+            };
+
+            return resultado;
         }
     }
 }
