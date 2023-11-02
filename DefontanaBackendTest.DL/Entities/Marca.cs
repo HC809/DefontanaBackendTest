978ca37add4fc61aaf35a5ec1373e2d4f4d00af2
Hector Caballero <caballero.22.809@gmail.com>
2023-11-01 19:44:36 -0600

    Add DataLayer (DBContext and scaffolded entities)

---
+++ DefontanaBackendTest.DL/Entities/Marca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefontanaBackendTest.DL.Entities;
+
+public partial class Marca
+{
+    public long IdMarca { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public virtual ICollection<Producto> Producto { get; set; } = new List<Producto>();
+}
---
+++ DefontanaBackendTest.DL/Entities/Venta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefontanaBackendTest.DL.Entities;
+
+public partial class Venta
+{
+    public long IdVenta { get; set; }
+
+    public int Total { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public long IdLocal { get; set; }
+
+    public virtual Local IdLocalNavigation { get; set; } = null!;
+
+    public virtual ICollection<VentaDetalle> VentaDetalle { get; set; } = new List<VentaDetalle>();
+}
